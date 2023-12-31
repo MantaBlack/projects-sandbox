1f10e30d622e6bf29f25abdb10e2b443633730f4
Nano Black <jude.ojiaku@hotmail.com>
2023-12-31 20:05:21 +0100

    Implement formula to draw a circle using vertices

--- vs2022/test-sfml/main.cpp
+++ vs2022/test-sfml/main.cpp
@@ -1,5 +1,8 @@
 #include <SFML/Graphics.hpp>
 
+#include <cmath>
+#include <iostream>
+
 static void test_sfml()
 {
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
@@ -81,10 +84,83 @@ static void draw_tutorial()
 }
 
 
+static void draw_circle()
+{
+    const float pi            = 22.f / 7.f;
+    const float end           = 2.f * pi;
+    const float radius        = 100.f;
+    const int num_triangles   = 12;
+    const int total_vertices  = num_triangles * 3;
+    const float sector_angle  = end / num_triangles;
+    const sf::Color colors[3] = {sf::Color::Red, sf::Color::Green, sf::Color::Blue};
+    
+    sf::RenderWindow window;
+
+    window.create(sf::VideoMode(800, 800),
+        "Draw Circle",
+        sf::Style::Default,
+        sf::ContextSettings(24));
+
+    window.setFramerateLimit(60);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::Black);
+
+        sf::VertexArray vertices(sf::Triangles);
+
+        const sf::Vector2f center(400.f, 400.f);
+        float theta = 0.f;
+        int c = 0;
+
+        for (; vertices.getVertexCount() < total_vertices; theta += sector_angle)
+        {
+            float x = std::cos(theta) * radius;
+            float y = std::sin(theta) * radius;
+
+            if (vertices.getVertexCount() == 0)
+            {
+                vertices.append(sf::Vertex(center, colors[c]));
+            }
+            else if (vertices.getVertexCount() < 3)
+            {
+                vertices.append(sf::Vertex(center + sf::Vector2f(x, y), colors[c]));
+            }
+            else
+            {
+                c += 1;
+
+                if (c >= 3)
+                {
+                    c = 0;
+                }
+
+                vertices.append(sf::Vertex(vertices[vertices.getVertexCount() - 1].position, colors[c]));
+                vertices.append(sf::Vertex(center, colors[c]));
+                vertices.append(sf::Vertex(center + sf::Vector2f(x, y), colors[c]));
+            }
+        }
+
+        window.draw(vertices);
+
+        window.display();
+    }
+}
+
+
 int main()
 {
-    //draw_tutorial();
-    test_sfml();
+    draw_circle();
 
     return 0;
 }
